42153e875562db9836b45b5ad49c8571ddb42333
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-08-26 14:06:41 +0200

    Created and designed a features section

--- index.php
+++ index.php
@@ -20,3 +20,23 @@ include 'inc/top.php';
         </div>
     </div>
 </div>
+<div class="homepageContainer">
+    <div class="homepageFeatures mt-3 mb-2">
+        <div class="homepageFeature">
+            <span class="featureIcon"><i class="fa fa-gear"></i></span class="featureIcon">
+            <h3 class="featureTitle">Editable Theme</h3>
+            <p class="featureDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, totam.</p>
+        </div>
+        <div class="homepageFeature">
+            <span class="featureIcon"><i class="fa fa-star"></i></span class="featureIcon">
+            <h3 class="featureTitle">Flat Design</h3>
+            <p class="featureDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, totam.</p>
+        </div>
+        <div class="homepageFeature">
+            <span class="featureIcon"><i class="fa fa-earth"></i></span class="featureIcon">
+            <h3 class="featureTitle">Audience</h3>
+            <p class="featureDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, totam
+                .</p>
+        </div>
+    </div>
+</div>
